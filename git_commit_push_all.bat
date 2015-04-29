12a3b0cc126828644599b10e2835bc270fbea9fd
asallab <ahmad.elsallab@gmail.com>
2015-04-29 13:21:18 +0200

    Working_lexicon_eshrag_twitter

--- git_commit_push_all.bat
+++ git_commit_push_all.bat
@@ -1,6 +1,6 @@
 git submodule foreach git add *
-git submodule foreach "git commit -m%1 || :"
+git submodule foreach "git commit -m'%1' || :"
 git submodule foreach git push origin HEAD:master
 git add *
-git commit -m%1
+git commit -m"%1"
 git push origin master:master
